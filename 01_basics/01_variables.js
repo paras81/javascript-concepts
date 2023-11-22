d639094af706d245e9ac10db74cfb12e74b84739
paras81 <40751135+paras81@users.noreply.github.com>
2023-11-22 07:36:45 +0000

    variable

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 1445;
+let accountEmail = "test12@gmail.com";
+var accountPassword = "123456";
+accountCity = "Amd"
+
+// accountId = 2;
+console.log(accountId);
+
+accountEmail = "pc@pc.com";
+accountPassword = "332233232";
+accountCity = "Rajkot";
+
+console.table([accountEmail,accountPassword,accountCity]);

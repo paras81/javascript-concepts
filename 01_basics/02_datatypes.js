06854fe56d9bb447928e1cfeef60f17bdb3a4179
paras81 <40751135+paras81@users.noreply.github.com>
2023-11-22 10:46:00 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,19 @@
+"use strict" // treat all js code as newer version
+
+let name = "Paras";
+
+let age = 23;
+
+let isLoggedin = true;
+
+// number => 2 to power 53;
+// bigint 
+// String = "";
+// boolean = true/false;
+// null = standalone value;
+// undefined
+// symbol
+
+// object
+
+console.log(typeof null);

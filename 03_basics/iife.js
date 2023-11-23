8f553e5a1bb9372488c0ab8c736f20acbb9ef088
paras81 <40751135+paras81@users.noreply.github.com>
2023-11-23 07:07:10 +0000

    iife function

---
+++ 03_basics/iife.js
@@ -0,0 +1,11 @@
+// immedieatly invoked function 
+
+(function check() {
+    //named iife
+    console.log("Checking...")
+})();
+
+
+    ((name) => {
+        console.log(`DB CONNECTED ${name}`)
+    })("Paras")
